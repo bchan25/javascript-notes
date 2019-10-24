e4bc382fc527f15d50b4d2c5140e9bc8f423fa53
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-24 17:33:47 +0100

    Added basic js note files

--- basics/basics.js
+++ basics/basics.js
@@ -1,2 +1,50 @@
 // Use comments as Notes
 // Chrome Developer Tools a good debug or testing tool
+
+// Primitives Types
+
+// Numbers 2 3.4 -10
+// We can do Math
+var math = 3 + 4;
+// Modulo - Remainder operator %
+
+
+// Strings Single or Double Quotes
+var hello = "Hello World!";
+
+// Concatenation
+var join = "Ben" + "Chan";
+
+// Escape Character to have "" \
+
+// Length Property
+hello.length;
+
+// Individual Character Zero Index
+hello[1];
+
+
+// Varibles - Store data to use later. Think of it like a Label Box
+var firstName = "Ben";
+var age = 31;
+var string = "Hello " + firstName + "!";
+
+// JS has Dynamic Typing meaning variable types can change without problem.
+// Other programming lanuages aren't dynamic typing.
+// camelCasing variable names
+
+// Null and Undfined
+var undfinedVar; // Undfined because no value was ever set to it.
+firstName = null; // Nothing eg like game over in game clear player name.
+
+
+// Useful Built in Methods
+// Alert
+alert("Hello there!");
+
+// Console.log
+console.log("Great for debugging");
+
+// Prompt - Get a user to input a value from popup alert
+prompt("What is your age?");
+
