08c340dc42040c82b38ba5eb786a4c8a901e5a37
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-25 14:34:32 +0100

    Added objects notes

---
+++ objects/objects.js
@@ -0,0 +1,51 @@
+/*
+  Objects - List with Key/Value.
+
+  Can hold numbers, strings, boolean, arrays, objects,  functions
+
+  Other Lanuage call objects a dictionary
+*/
+
+var person = {
+  name: "Ben",
+  age: 31,
+  city: "Flint",
+  sayHi: function () {
+    console.log("Hi");
+  }
+};
+
+// Retrieving data (bracket notation or dot notation)
+console.log(person["name"]);
+console.log(person.age);
+person.sayHi();
+
+// Update Data
+person.age += 1;
+console.log(person.age);
+
+/*
+  Creating Objects
+
+  var person = {}
+
+  or
+
+  var person = new Object();
+
+  or like the one above
+*/
+
+// Common Array with Nested Objects
+var posts = [
+  {
+    title: "Hello World",
+    author: "Ben"
+  },
+  {
+    title: "Random Title",
+    author: "Ben"
+  }
+]
+
+// This Keyword (Which data you refer to)
