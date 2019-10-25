3d297f15e21585390f589d67c5d59bf4c321a937
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-25 12:45:57 +0100

    Added array methods

--- array/array.js
+++ array/array.js
@@ -15,3 +15,27 @@ friends[0] = "Chris";
 // Add new data (Index not used yet)
 friends[4] = "Kate"
 
+/*
+  Array Methods
+*/
+
+var colours = ["red", "orange", "yellow"];
+
+// Add new data to end
+colours.push("blue");
+
+// Remove last element of Array
+colours.pop();
+
+// Add new data to front of Array
+colours.unshift("green");
+
+// Remove first element of array
+colours.shift();
+
+// Find the index of an item (first instance)
+colours.indexOf("red");
+
+// Slice to copy parts of an array [orange, yellow]
+// Inclusive means not including the last number (index)
+var newColours = colours.slice(1, 3);
