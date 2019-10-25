bf4cbb364ef249e2275e193f7734f47ad7ccda5e
Benjamin Chan <benchan_2000@hotmail.com>
2019-10-25 12:24:00 +0100

    Added basic array notes

---
+++ array/array.js
@@ -0,0 +1,17 @@
+/*
+  Arrays - Data Structure
+  A list of data that is group together
+*/
+
+// Array - Zero index [0] [1] [2] [3]
+var friends = ["Charlie", "Liz", "David", "Helen"];
+
+// Hello Liz
+console.log("Hello " + friends[1]);
+
+// Update a value
+friends[0] = "Chris";
+
+// Add new data (Index not used yet)
+friends[4] = "Kate"
+
